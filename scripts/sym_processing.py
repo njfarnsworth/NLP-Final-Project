e7bd11efb283b5a7edad729ea2966d36b8cc7e51
Nayda Farnsworth <125708157+njfarnsworth@users.noreply.github.com>
2025-10-31 08:23:06 -0400

    Adding MWE files for symmetry and cross-evaluation

---
+++ scripts/sym_processing.py
@@ -0,0 +1,18 @@
+#pseudocode
+
+# import the asymmetry dataset from Huggingface
+
+# DO THIS PROCESS FOR THE TRAINING AND TESTING SET TO CREATE TWO TSVS, ONE FOR TRAINING AND ONE FOR EVALUATION 
+# create a blank tsv 
+
+#for i in len(data):
+# set sent[i] as text
+# set sent[i+1] as pair
+# if sent[i][num] == sent[i+1][num]
+#     condition = symmetric
+#     set entailment
+# else
+#     condition = asymmetric 
+#     set neutral 
+
+# return the tsv 
